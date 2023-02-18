83c2a5b983f33489a98316d5314a0104bd8c2dbf
Nikshit Chauhan <nikshit2012@gmail.com>
2023-02-18 14:18:33 +0530

    Added firebase authentication

--- app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
@@ -1,13 +1,51 @@
 package com.example.kotlinmessenger
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val btnLogin : Button = findViewById(R.id.btn_login)
+        val haveAccount : TextView = findViewById(R.id.tv_have_account)
+
+        btnLogin.setOnClickListener {
+            val loginEmail = findViewById<EditText>(R.id.ed_email_login).text.toString()
+            val loginPass = findViewById<EditText>(R.id.ed_password_login).text.toString()
+
+            Log.d("Login ", "Attempted with email $loginEmail")
+            Log.d("Login ", "Attempted with password $loginPass")
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(loginEmail,loginPass)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) {
+                        Log.d("firebase", "Login failed")
+                        return@addOnCompleteListener
+                    }
+
+                    //else if successful
+                    Log.d("firebase", "Logged In Successfully")
+                }
+                .addOnFailureListener {
+                    Log.d("firebase", "Failed to Login  ${it.message}")
+                    Toast.makeText(this, "Failed to login ${it.message}", Toast.LENGTH_LONG).show()
+                }
+        }
+
+        haveAccount.setOnClickListener {
+            finish()
+        }
+
+
 
     }
 
--- app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
@@ -7,23 +7,32 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.NonNull
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val email : String? = null
+    private val password : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
         val btnRegister : Button = findViewById(R.id.btn_register)
         val haveAccount : TextView = findViewById(R.id.tv_have_account)
 
         btnRegister.setOnClickListener{
-            val email = findViewById<EditText>(R.id.ed_email_register).text.toString()
-            val password = findViewById<EditText>(R.id.ed_password_register).text.toString()
-
-            Log.d("Main Activity", "Email.is: $email")
-            Log.d("Main Activity", "Password: $password")
+            performRegister()
         }
         haveAccount.setOnClickListener{
             Log.d("Main Activity", "clicked on already have an account!!")
@@ -32,7 +41,37 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
-
-
     }
+
+    private fun performRegister() {
+        val email = findViewById<EditText>(R.id.ed_email_register).text.toString()
+        val password = findViewById<EditText>(R.id.ed_password_register).text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter email /Password", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        Log.d("Main Activity", "Email.is: $email")
+        Log.d("Main Activity", "Password: $password")
+
+        //Firebase Authentication to create new User with email and password
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener{
+                if(!it.isSuccessful) {
+                    Log.d("firebase", "User NOt created")
+                    return@addOnCompleteListener
+                }
+
+                //else if successful
+                Log.d("firebase", "User created with uid ${it.result.user?.uid}")
+            }
+            .addOnFailureListener {
+                Log.d("firebase", "Failed to create User ${it.message}")
+                Toast.makeText(this, "Failed to create User ${it.message}", Toast.LENGTH_LONG).show()
+            }
+    }
+
+
 }
